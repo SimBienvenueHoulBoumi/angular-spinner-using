94ce0668794b2277d123acf5cfc44e9e038c7d9c
Sim <64483242+SimBienvenueHoulBoumi@users.noreply.github.com>
2024-02-18 12:44:45 +0100

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { PageOneComponent } from './page-one/page-one.component';
+import { PageTwoComponent } from './page-two/page-two.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'page-one',
+    pathMatch: 'full',
+  },
+  {
+    path: 'page-one',
+    component: PageOneComponent,
+  },
+  {
+    path: 'page-two',
+    component: PageTwoComponent,
+  },
+  {
+    path: 'page-not-found',
+    component: PageNotFoundComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/page-not-found',
+  },
+];
